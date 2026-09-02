ea8f292eb5678da2e54b2adbaa3d320e3443a912
Elvis <sestomielvis@gmail.com>
2026-09-02 12:32:44 +0700

    modified allowed hosts

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -15,9 +15,13 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 # Load environment variables from .env file
-load_dotenv()
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "elvis-sestomi-myportofolio.pws.cs.ui.ac.id"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "elvis-sestomi-myportofolio.pws.cs.ui.ac.id",
+]
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
