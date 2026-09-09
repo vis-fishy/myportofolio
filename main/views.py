55721b35bdb564c386c2ab84ebd16ca0e976437f
Elvis <sestomielvis@gmail.com>
2026-09-09 17:02:21 +0700

    build: finished build main app

--- main/views.py
+++ main/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Elvis",
+        "npm": "2506544990",
+        "study_program": "S1 Ilmu Komputer",
+        "bio": (
+            "CS student at Universitas Indonesia, Expected to graduate in 2029. Interested in Cyber Security and Web Development"
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Burhan",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
